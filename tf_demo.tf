d451fc2eb1bb2e639ab614f54a5decbda7f916ff
Parasaram <38139630+Parasaram@users.noreply.github.com>
2019-12-18 02:09:37 +0530

    Add files via upload

---
+++ tf_demo.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "AKIAJ35VRAIQFF3PV2FA"
+  secret_key = "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"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  key_name = "key1"
+}
